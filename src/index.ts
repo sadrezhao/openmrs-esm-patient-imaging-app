2b58d77e94355bc6c608a1c34e4981158f4f5911
sadrezhao <wei.zhao@uclouvain.be>
2025-05-07 13:07:13 +0200

    Change package name

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,7 @@ import { dashboardMeta } from './dashboard.meta';
 import { configSchema } from './config-schema';
 import ImagingDetailedSummaryComponent from './imaging/imaging-summary/imaging-detailed-summary.component';
 
-// const moduleName = '@openmrs/esm-patient-imaging-app';
-const moduleName = '@zhaosadre/openmrs-esm-patient-imaging-app';
+const moduleName = '@openmrs/esm-patient-imaging-app';
 
 const options = {
   featureName: 'patient-imaging',
