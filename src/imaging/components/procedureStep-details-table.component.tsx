f32e1732673be06e3438082654e7ce3b2be561f7
sadrezhao <wei.zhao@uclouvain.be>
2025-12-12 00:09:27 +0100

    Fix bug

--- src/imaging/components/procedureStep-details-table.component.tsx
+++ src/imaging/components/procedureStep-details-table.component.tsx
@@ -194,7 +194,7 @@ const ProcedureStepTable: React.FC<ProcedureStepTableProps> = ({ requestProcedur
                     <React.Fragment key={rowIndex}>
                       <TableRow>
                         {row.cells.map((cell, cellIndex) => (
-                          <TableCell className={styles.tableCell} key={cellIndex} style={{ marginLeft: '20px' }}>
+                          <TableCell className={styles.tableCell} key={cellIndex}>
                             {cell.value?.content ?? cell.value}
                           </TableCell>
                         ))}
